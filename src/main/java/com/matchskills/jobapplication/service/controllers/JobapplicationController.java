92389cb100ddb15734955f9dba5f5a87015413ed
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-08-19 09:26:25 -0300

    refactor: create new CurriculumDomain for upload curriculum

--- src/main/java/com/matchskills/jobapplication/service/controllers/JobapplicationController.java
+++ src/main/java/com/matchskills/jobapplication/service/controllers/JobapplicationController.java
@@ -12,6 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -31,7 +32,7 @@ public class JobapplicationController {
     public ResponseEntity<JobApplicationResponse> createJobaplication(
             @RequestPart("curriculum") MultipartFile curriculum,
             @RequestPart("data") CreateJobapplicationRequest createJobapplicationRequest
-    ){
+    ) throws IOException {
         return ResponseEntity.status(HttpStatus.CREATED).body(jobapplicationService.createJobApplication(createJobapplicationRequest, curriculum));
     }
 
